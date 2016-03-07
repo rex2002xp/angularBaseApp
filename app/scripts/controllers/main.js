e4cea7df2ad127e8dc7c9e54bf6b44323fc8b011
Victor Cornejo <rex2002xp@gmail.com>
2016-03-07 14:44:29 -0600

    Creacion de Proyecto.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularBaseApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularBaseApp
+ */
+angular.module('angularBaseApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
